72e4ee672dfaf19b8a940f963d89a59ef98c1793
Edinson Madrid <edinsonmadrid@gmail.com>
2025-07-14 23:34:40 -0300

    update page home redirect to docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-
-import Link from 'next/link';
+'use client'
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-		<main className='flex min-h-screen flex-col items-center justify-center p-24'>
-			<div className='text-center'>
-				<h1 className='text-4xl font-bold'>
-					Plantilla de API de Next.js
-				</h1>
-				<p className='mt-4 text-lg'>
-					Esta es una plantilla para crear API RESTful con Next.js,
-					MongoDB y Swagger. .
-				</p>
-				<Link
-					href='/docs'
-					legacyBehavior>
-					<a className='mt-6 inline-block rounded-md bg-blue-600 px-6 py-3 text-white font-semibold hover:bg-blue-700'>
-						Ver la documentación de la API
-					</a>
-				</Link>
-			</div>
-		</main>
-  );
-}
+	const router = useRouter();
+	useEffect(() => {
+		router.push('/docs');
+	}, [router]);
+
+	return null;
+
+};
